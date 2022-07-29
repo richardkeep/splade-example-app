0746d085ccfac73e2f9fb96903ecaa12496d88bc
richardkeep <r.kipsang@gmail.com>
2022-07-29 21:36:29 +0300

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Splade::share('adminLastSeenAt', now());
-        Splade::share('users', User::all());
+        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use ProtoneMedia\Splade\Facades\Splade;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 use App\Http\Controllers\UserController;
 
 Route::middleware(['splade'])->group(function () {
+    Splade::share('users', User::all());
     Route::resource('users', UserController::class);
     Route::get('/', fn () => view('home'))->name('home');
     Route::get('/demo', fn () => view('demo'))->name('demo');
